68d4f98c93a13441be07acf30bde548c004fdce8
T3rex <31337aditya@gmail.com>
2025-06-07 16:28:18 +0530

    created pricingmodule model

--- pricing_engine/core/models.py
+++ pricing_engine/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class PricingModule(models.Model):
+    name = models.CharField(max_length=100)
+    is_Active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
